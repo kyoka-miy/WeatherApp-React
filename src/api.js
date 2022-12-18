7cd0f7eec2532703b64a551fab410d2c4f27f468
Kyoka Miyamura <kyoka.miyamura21@gmail.com>
2022-12-18 00:19:29 -0800

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd5022f5972mshe1715ad98486287p162fc7jsnb2765543aeba',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/';
+export const WEATHER_API_KEY = '9c763e822229cf90acafe9937bd721bb';
